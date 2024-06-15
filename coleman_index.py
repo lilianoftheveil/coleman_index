ccb37bf9e0a7c6544797c35238382b4cbcbdf45c
Lilian Oliveira <92271274+lilianoftheveil@users.noreply.github.com>
2024-06-15 17:11:35 +0100

    Add files via upload

--- coleman_index.py
+++ coleman_index.py
@@ -1,41 +1,42 @@
 # -*- coding: utf-8 -*-
 
-x = input("Digit your text: ")
+def col(x):
+    w = 1                                                   # Number of words
+    for a in x:
+        if a == " ":
+            w = w + 1
+    s = 0                                                   # Number of sentences
+    for b in x:
+        if b == "." or b == "!" or b == "?" or b == "...":
+            s = s + 1
+    l = 0                                                   # Number of letters
+    for c in x:        # PYTHON Documentation (Utilities for ASCII characters - that check all letters lower and upper -)
+        if c.isalpha():
+            l = l + 1
+    cl = (l / w * 100)
+    cs = (s / w * 100)
+    calcu = round((0.0588 * cl) - (0.296 * cs) - 15.8)      # Coleman-Liau index
+    if calcu < 1:                                           # Print of the Before Grade 1
+        print("Índice de Legibilidade: Pré-escolar")
+    if calcu == 1 or calcu > 1 and calcu <= 16:             # Print of the Grade 1
+        print(f"Índice de Legibilidade: {calcu}")                                        
+    if calcu > 16:                                          # Print of the Grade more than 16
+        print("Índice de Legibilidade: 16+")
 
-# Number of words
-w = 1
-for a in x:
-    if a == " ":
-        w = w + 1
+    return((f"Número de Palavras: {w}"), (f"Número de Setenças: {s}"), (f"Número de Letras: {l}"))
 
-# Number of sentences
-s = 0
-for b in x:
-    if b == "." or b == "!" or b == "?" or b == "...":
-        s = s + 1
 
-# Number of letters
-l = 0
-for c in x:
-    # PYTHON Documentation (Utilities for ASCII characters - that check all letters lower and upper -)
-    if c.isalpha():
-        l = l + 1
-
-# Coleman-Liau index
-cl = (l / w * 100)
-cs = (s / w * 100)
-calcu = round((0.0588 * cl) - (0.296 * cs) - 15.8)
-
-# Print of the Before Grade 1
-if calcu < 1:
-    print("Before Grade 1")
-
-# Print of the Grade 1
-if calcu == 1 or calcu > 1 and calcu <= 16:
-    print(f"Grade {calcu}")
-
-# Print of the Grade more than 16
-if calcu > 16:
-  print("Grade 16+")
-
-print(w)
+while True:
+    x = input("digite 'c' para escrever um texto ou 't' para abrir arquivo .txt: ").lower()
+    if x == "t":
+        y = input("caminho: ")  
+        z = open(y).read()
+        print(col(z))
+        break
+    elif x == "c":
+        y = input("texto: ")    
+        print(col(y))
+        break
+    else:
+        print("entrada inválida.")
+        continue
